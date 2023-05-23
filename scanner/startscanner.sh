a1d410804b8ae2112d02e7368cfbb605f83e3c7f
leobuchman <lbuchman@sputnik-em.com>
2023-05-23 15:01:31 -0400

    fixed event number

--- scanner/startscanner.sh
+++ scanner/startscanner.sh
@@ -3,7 +3,7 @@
 EVENT_DEV=$(cat /proc/bus/input/devices | awk '/SCANNER/{for(a=0;a>=0;a++){getline;{if(/kbd/==1){ print $NF;exit 0;}}}}')
 
 if [ -z "$EVENT_DEV" ]; then
-    EVENT_DEV=$(cat /proc/bus/input/devices | awk '/Scanner/{for(a=0;a>=0;a++){getline;{if(/kbd/==1){ print $NF;exit 0;}}}}')
+    EVENT_DEV=$(cat /proc/bus/input/devices | awk '/Scanner/{for(a=0;a>=0;a++){getline;{if(/kbd/==1){ print $4;exit 0;}}}}')
 fi
 
 if [ -z "$EVENT_DEV" ]; then
