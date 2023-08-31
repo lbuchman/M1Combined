b0e860246954f2e58652742e19715ec6e90e70c6
leobuchman <lbuchman@sputnik-em.com>
2023-08-31 00:03:39 -0400

    spell fix

--- scanner/startscanner.sh
+++ scanner/startscanner.sh
@@ -2,10 +2,7 @@
 #sleep 30
 
 
-if [ -z "$EVENT_DEV" ]; then
-    sleep 1
-    exit 1
-fi
-SNANNER_DEV=$(jq -r '.snapperDev' /var/snap/m1tfd1/current/config.json)
+// cat /proc/bus/input/devices
+SNANNER_DEV=$(jq -r '.scannerDev' /var/snap/m1tfd1/current/config.json)
 echo "sudo $SNAP/usr/local/bin/scanner -e $SNANNER_DEV"
 sudo $SNAP/usr/local/bin/scanner -e $SNANNER_DEV
