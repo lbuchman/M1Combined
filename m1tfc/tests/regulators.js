517c6b72716bc5c287188851ee65a1dd5e814792
lbuchman <leo.buchman@carrier.com>
2023-09-06 16:32:47 -0400

    fixed coincell tolerance

--- m1tfc/tests/regulators.js
+++ m1tfc/tests/regulators.js
@@ -12,7 +12,7 @@ const testPoints = [
     { name: 'TP36', voltage: 1.2 },
     { name: 'J5.13', voltage: 11.7 },
     { name: 'J5.8', voltage: 6.0 },
-    { name: 'BatCellBat', voltage: 3.0, tolerance: 0.1 }
+    { name: 'BatCellBat', voltage: 3.0, tolerance: 0.15 }
 ];
 
 async function testDDRVoltage(tolerance, logger) {
