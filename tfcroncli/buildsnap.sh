376a184babfef6ae81cd0688b0cf79980e3c760a
lbuchman <leo.buchman@carrier.com>
2023-10-04 23:38:45 -0400

    work

--- tfcroncli/buildsnap.sh
+++ tfcroncli/buildsnap.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-COMMITID=`git log -n1 --format="%h"`
+export COMMITID=`git log -n1 --format="%h"`
 echo commitid = $COMMITID
-sed -i "/version:   /c\version: \"$COMMITID"\" ./snap/snapcraft.yaml 
+sed -i "s/^version:.*/version: \"$COMMITID\"/"  ./snap/snapcraft.yaml
 snapcraft
 
